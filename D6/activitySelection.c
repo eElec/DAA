cd204212591feaae6835506c72533f0c7a333def
Adrish Aditya <adrishaditya@gmail.com>
2020-09-05 10:21:03 +0530

    added: d6

---
+++ D6/activitySelection.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdio.h>
+
+typedef struct{
+	int startTime;
+	int endTime;
+}Activity;
+
+void swap(Activity *a, Activity *b){
+	Activity temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void heapify(Activity *ac, int n, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && ac[left].endTime > ac[largest].endTime)
+		largest = left;
+
+	if (right < n && ac[right].endTime > ac[largest].endTime)
+		largest = right;
+
+	if (largest != i)
+	{
+		swap(&ac[i], &ac[largest]);
+		heapify(ac, n, largest);
+	}
+}
+
+void heapSort(Activity *ac, int n)
+{
+	// Build heap
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(ac, n, i);
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		swap(&ac[0], &ac[i]);
+		heapify(ac, i, 0);
+	}
+}
+
+void main(){
+	int n;
+	printf("Enter number of activities: ");
+	scanf("%d", &n);
+
+	Activity ac[n];
+	printf("Enter Activity Details: \n");
+	for(int i=0; i<n; i++){
+		printf("Activity %d\n", i+1);
+		printf("Enter Start Time: ");
+		scanf("%d", &ac[i].startTime);
+		printf("Enter End Time: ");
+		scanf("%d", &ac[i].endTime);
+	}
+
+	//O(nlogn) sort
+	heapSort(ac, n);
+
+	int count = 1;
+	int curr = 0;
+	for(int i=1; i<n; i++){
+		if(ac[curr].endTime <= ac[i].startTime){
+			count++;
+			curr = i;
+		}
+	}
+	printf("Number of Activities that can be performed: %d\n", count);
+}
---
+++ D6/knapsack.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	float weight;
+	float price;
+	float priceDensity;
+}Knapsack;
+
+void swap(Knapsack *a, Knapsack *b){
+	Knapsack temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void heapify(Knapsack *ks, int n, int i)
+{
+	int smallest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && ks[left].priceDensity < ks[smallest].priceDensity)
+		smallest = left;
+
+	if (right < n && ks[right].priceDensity < ks[smallest].priceDensity)
+		smallest = right;
+
+	if (smallest != i)
+	{
+		swap(&ks[i], &ks[smallest]);
+		heapify(ks, n, smallest);
+	}
+}
+
+void heapSort(Knapsack *ks, int n)
+{
+	// Build heap
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(ks, n, i);
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		swap(&ks[0], &ks[i]);
+		heapify(ks, i, 0);
+	}
+}
+void main(){
+	int n;
+	printf("Enter Number of Items: ");
+	scanf("%d", &n);
+
+	Knapsack ks[n];
+	printf("Enter information: \n");
+	for(int i=0; i<n; i++){
+		printf("Item %d: \n", i+1);
+		printf("Enter Weight: ");
+		scanf("%f", &ks[i].weight);
+		printf("Enter Price: ");
+		scanf("%f", &ks[i].price);
+
+		ks[i].priceDensity = ks[i].price / ks[i].weight;
+	}
+
+	float MaxWeight;
+	printf("Enter Max Weight: ");
+	scanf("%f", &MaxWeight);
+
+	//O(nLogn) sort
+	heapSort(ks, n);
+
+	float MaxPrice = 0;
+	int i;
+	for( i=0; i<n; i++){
+		if(MaxWeight > 0 && ks[i].weight <= MaxWeight){
+			MaxWeight -= ks[i].weight;
+			MaxPrice += ks[i].price;
+		}
+		else{
+			break;
+		}
+	}
+	if(MaxWeight > 0){
+		MaxPrice += ks[i].price*(MaxWeight/ks[i].weight);
+	}
+
+	printf("Max Price: %f\n", MaxPrice);
+}
